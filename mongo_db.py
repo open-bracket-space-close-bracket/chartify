6ef126e6d6a10adbfefc0f8ca9e484cbaa1b5558
James Brooks <brooksjamesk01@gmail.com>
2022-05-17 17:41:55 -0700

    integrated MongoDB and get 3 of 4 methods working

--- mongo_db.py
+++ mongo_db.py
@@ -1,26 +1,85 @@
-import mongoengine as db
+import json
+from flask import Flask, jsonify, make_response, request
+from flask_mongoengine import MongoEngine
 
-database_name = "401mid"
-DB_URI = f"mongodb+srv://401_mid_group:12345@401cluster.4us9bdk.mongodb.net/{database_name}?retryWrites=true&w=majority"
-db.connect(DB_URI)
+app = Flask(__name__)
+
+database_name = "401Mid"
+DB_URI = f"mongodb+srv://DJLMT:12345@quickstartusers.zmxlh.mongodb.net/{database_name}?retryWrites=true&w=majority"
+app.config["MONGODB_HOST"] = DB_URI
 
 
-class Users(db.Document):
-  id = db.IntField()
+db = MongoEngine()
+db.init_app(app)
+
+class users(db.Document):
   user_email = db.StringField()
   user_name = db.StringField()
   user_queries = db.StringField()
 
   def to_json(self): 
     return {
-      "id": self.id,
       "user_email": self.user_email,
       "user_name": self.user_name,
       "user_queries": self.user_queries
     }
-  
-test_user = Users(id=1, user_email="a@gmail.com", user_name="aa", user_queries="a, a")
 
-test_user.save()
-db_query = Users.objects(id=1).first()
-print(db_query.to_json())
+@app.route('/user/add', methods=["POST"])
+def user_add(email,name):
+  user = users(user_email=email, user_name=name, user_queries="")
+  user.save()
+  return make_response("",201)
+
+
+# @app.route('/user/print', methods=["GET", "POST"])
+# def user_print():
+#   print()
+#   if request.method == 'GET':
+#     pass
+#     users_list = []
+#     print(users)
+#     for user in users.objects:
+#       users_list.append(user)
+#     # return str(users_list[0]["id"])
+#     return make_response(jsonify(users_list),200)
+#   elif request.method == "POST":
+#     content = request.json
+#     user = users(user_email=content["user_email"],user_name=content['user_name'],user_queries=content["user_queries"],)
+#     user.save()
+#     return make_response("", 201)
+
+@app.route('/user/print/<user_id>/<new_coin>', methods=["GET","PUT","DELETE"])
+def each_user_functions(user_id, new_coin):
+  if request.method == "GET":
+    user_obj = users.objects(id=user_id).first()
+    if user_obj:
+      return make_response(jsonify(user_obj.to_json()),200)
+    else:
+      return make_response("", 404)
+  elif request.method == "PUT":
+    content = request.json
+    queries = content['user_queries']
+    new_queries = queries + f", {new_coin}"
+    print(new_queries)
+    user_obj = users.objects(id=user_id).first()
+    user_obj.update(user_email=content['user_email'], user_name=content['user_name'], user_queries=new_queries)
+    return make_response('', 204)
+  elif request.method == "DELETE":
+    user_obj = users.objects(id=user_id).first()
+    user_obj.delete()
+    return make_response("", 410)
+
+
+if __name__=="__main__":
+  app.run()
+
+# db.connect(host=DB_URI)
+
+
+
+  
+
+
+
+# db_query = Users.objects(id=1).first()
+# print(db_query.to_json())
