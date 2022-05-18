aa9a0ce43f3247c99206a104891f05cb79f59d52
James Brooks <brooksjamesk01@gmail.com>
2022-05-18 13:17:11 -0700

    got POST route to work and attempted for a better PUT route

--- mongo_db.py
+++ mongo_db.py
@@ -10,12 +10,13 @@ app.config["MONGODB_HOST"] = DB_URI
 
 
 db = MongoEngine()
+db.database_name = database_name
 db.init_app(app)
 
 class users(db.Document):
   user_email = db.StringField()
   user_name = db.StringField()
-  user_queries = db.StringField()
+  user_queries = db.ListField()
 
   def to_json(self): 
     return {
@@ -24,29 +25,31 @@ class users(db.Document):
       "user_queries": self.user_queries
     }
 
+
 @app.route('/user/add', methods=["POST"])
-def user_add(email,name):
-  user = users(user_email=email, user_name=name, user_queries="")
-  user.save()
-  return make_response("",201)
+def user_add():
+  if request.method == "POST":
+    data = request.json
+    user = users(user_email=data['user_email'], user_name=data['user_name'], user_queries=[])
+    user.save()
+    return make_response("",201)
 
 
-# @app.route('/user/print', methods=["GET", "POST"])
-# def user_print():
-#   print()
-#   if request.method == 'GET':
-#     pass
-#     users_list = []
-#     print(users)
-#     for user in users.objects:
-#       users_list.append(user)
-#     # return str(users_list[0]["id"])
-#     return make_response(jsonify(users_list),200)
-#   elif request.method == "POST":
-#     content = request.json
-#     user = users(user_email=content["user_email"],user_name=content['user_name'],user_queries=content["user_queries"],)
-#     user.save()
-#     return make_response("", 201)
+@app.route('/user/print', methods=["GET", "POST"])
+def user_print():
+  if request.method == 'GET':
+    pass
+    users_list = []
+    print(users)
+    for user in users.objects:
+      users_list.append(user)
+    # return str(users_list[0]["id"])
+    return make_response(jsonify(users_list),200)
+  elif request.method == "POST":
+    content = request.json
+    user = users(user_email=content["user_email"],user_name=content['user_name'],user_queries=content["user_queries"],)
+    user.save()
+    return make_response("", 201)
 
 @app.route('/user/print/<user_id>/<new_coin>', methods=["GET","PUT","DELETE"])
 def each_user_functions(user_id, new_coin):
@@ -57,12 +60,14 @@ def each_user_functions(user_id, new_coin):
     else:
       return make_response("", 404)
   elif request.method == "PUT":
-    content = request.json
-    queries = content['user_queries']
-    new_queries = queries + f", {new_coin}"
-    print(new_queries)
-    user_obj = users.objects(id=user_id).first()
-    user_obj.update(user_email=content['user_email'], user_name=content['user_name'], user_queries=new_queries)
+    # content = request.json
+    # queries = content['user_queries']
+    # print(type(queries))
+    # print(new_coin)
+    db.updateOne({"_id": f"{user_id}"}, {"$push": {"user_queries": new_coin}})
+    # print(new_queries)
+    # user_obj = users.objects(id=user_id).first()
+    # user_obj.update(user_email=content['user_email'], user_name=content['user_name'], user_queries=new_queries)
     return make_response('', 204)
   elif request.method == "DELETE":
     user_obj = users.objects(id=user_id).first()
