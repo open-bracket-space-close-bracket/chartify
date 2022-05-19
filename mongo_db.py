63195e4f2f1259754a9c80f6bb43c92d3dc7c489
James Brooks <brooksjamesk01@gmail.com>
2022-05-18 17:24:31 -0700

    moved routes to app.py and added parameters to pass information properly

--- mongo_db.py
+++ mongo_db.py
@@ -2,16 +2,16 @@ import json
 from flask import Flask, jsonify, make_response, request
 import requests
 from flask_mongoengine import MongoEngine
+import os
 
 app = Flask(__name__)
 
-database_name = "401Mid"
-DB_URI = f"mongodb+srv://DJLMT:12345@quickstartusers.zmxlh.mongodb.net/{database_name}?retryWrites=true&w=majority"
+DB_URI = os.environ.get("DB_URI",None)
 app.config["MONGODB_HOST"] = DB_URI
 
 
 db = MongoEngine()
-db.database_name = database_name
+# db.database_name = database_name
 db.init_app(app)
 
 class users(db.Document):
@@ -26,11 +26,6 @@ class users(db.Document):
       "user_queries": self.user_queries
     }
 
-
-## figure out how to get a user's _ID on mongo as they're added
-## figure out how to get a user's _ID on mongo once they log in (assuming they already have a mongoDB _ID)
-
-
 def get_all_ids(): 
   users_dict = {}
   for user in users.objects:
@@ -43,22 +38,22 @@ def compare_and_find(user_email=None):
   user = ids[user_email]
   return user
 
-@app.route('/users/ids')
-def do_things():
+
+def do_things(request):
   if request.method == "GET":
     data = request.json
     try:
       user = compare_and_find(data["user_email"])
     except:
-      url = "http://127.0.0.1:5000/user/add"
+      url = "https://djlmt-chartify.herokuapp.com/user/add"
       data = {"user_email": data["user_email"], "user_name": data['user_name']}
       requests.post(url, json= data)
-      response = requests.get("http://127.0.0.1:5000/users/ids", json=data)
+      response = requests.get("https://djlmt-chartify.herokuapp.com/users/ids", json=data)
       return response._content
     return str(user)
 
-@app.route('/user/add', methods=["POST"])
-def user_add():
+
+def user_add(request):
   if request.method == "POST":
     data = request.json
     user = users(user_email=data['user_email'], user_name=data['user_name'], user_queries="")
@@ -66,39 +61,40 @@ def user_add():
     return make_response("",201)
 
 
-@app.route('/user/print', methods=["GET", "POST"])
-def user_print():
+
+def user_print(request):
   if request.method == 'GET':
     users_list = []
     for user in users.objects:
       users_list.append(user)
     return make_response(jsonify(users_list),200)
-  elif request.method == "POST":
-    content = request.json
-    user = users(user_email=content["user_email"],user_name=content['user_name'],user_queries=content["user_queries"],)
-    user.save()
-    return make_response("", 201)
+  # elif request.method == "POST":
+  #   content = request.json
+  #   user = users(user_email=content["user_email"],user_name=content['user_name'],user_queries=content["user_queries"],)
+  #   user.save()
+  #   return make_response("", 201)
 
-@app.route('/user/print/<user_id>/<new_coin>', methods=["GET","PUT","DELETE"])
-def each_user_functions(user_id, new_coin):
+
+def each_user_functions(user_id, new_coin, request):
   if request.method == "GET":
     user_obj = users.objects(id=user_id).first()
     if user_obj:
       return make_response(jsonify(user_obj.to_json()),200)
     else:
       return make_response("", 404)
+
   elif request.method == "PUT":
     content = request.json
     queries = content['user_queries']
     if queries == "": 
       new_queries = queries + new_coin
-    # print(type(queries))
-    # print(new_coin)
     else: 
       new_queries = queries + f", {new_coin}"
     user_obj = users.objects(id=user_id).first()
     user_obj.update(user_email=content['user_email'], user_name=content['user_name'], user_queries=new_queries)
     return make_response('', 204)
+
+  # REWORK -- To get it to delete from the user_queries rather than the entire Entry.
   elif request.method == "DELETE":
     user_obj = users.objects(id=user_id).first()
     user_obj.delete()
@@ -107,14 +103,3 @@ def each_user_functions(user_id, new_coin):
 
 if __name__=="__main__":
   app.run()
-
-# db.connect(host=DB_URI)
-
-
-
-  
-
-
-
-# db_query = Users.objects(id=1).first()
-# print(db_query.to_json())
